30ffdb9f5884c9d8c410682fdf9ce6bfe274f0bf
crcuevas <31038722+crcuevas@users.noreply.github.com>
2017-08-15 09:34:49 -0400

    Add files via upload

---
+++ WayFinder/WayFinder/javascript/WF_PopUp.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+var popupDiv = document.getElementById("popup_bg")
+
+function openPopupMenue(){
+	popupDiv.style.display="block";
+}
+
+function closePopupMenue () {
+	popupDiv.style
+}
